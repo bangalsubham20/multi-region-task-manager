6283a6d8245af42cdfa087b657ffd1002e237dc3
Subham Bangal <sbangal354@gmail.com>
2026-08-05 14:10:08 +0530

    Created Structure

---
+++ backend/src/main/java/com/multiregion/taskmanager/MultiRegionTaskManagerApplication.java
@@ -0,0 +1,12 @@
+package com.multiregion.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiRegionTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiRegionTaskManagerApplication.class, args);
+    }
+}
